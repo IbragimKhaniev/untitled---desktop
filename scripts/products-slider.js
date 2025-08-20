96b959c4ed4f9cae6d5f877f1f91c87c77589d2f
IbragimKhaniev <newinterny@gmail.com>
2025-08-20 21:32:05 +0300

    great

---
+++ scripts/products-slider.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function() {
+const swiper = new Swiper('.team-section__slider .swiper', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  loop: true,
+  spaceBetween: 30,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 200,
+    modifier: 1,
+    slideShadows: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      coverflowEffect: {
+        depth: 100,
+        modifier: 0.8,
+      }
+    },
+    768: {
+      coverflowEffect: {
+        depth: 150,
+        modifier: 1,
+      }
+    },
+    1024: {
+      coverflowEffect: {
+        depth: 200,
+        modifier: 1.2,
+      }
+    }
+  }
+});
+});
